e83e73fb23384a1d4090ba3a16b1714d7279c6dd
Juan <juandavicard2008@gmail.com>
2025-05-20 19:38:14 -0500

    More dependencies updated

--- serenity-junit-starter-master/src/test/java/starter/steps/MathWizSteps.java
+++ serenity-junit-starter-master/src/test/java/starter/steps/MathWizSteps.java
@@ -2,22 +2,25 @@ package starter.steps;
 
 import net.thucydides.core.annotations.Step;
 
-public class MathWizSteps {
-
+public class MathWizSteps
+{
    String actor;
 
     @Step("#actor tiene {0}")
-    public void tiene(int amount) {
+    public void tiene(int amount)
+    {
         // TODO
     }
 
     @Step("#actor agrega {0}")
-    public void suma(int amount) {
+    public void suma(int amount)
+    {
         // TODO
     }
 
     @Step("#actor deberia tener {0}")
-    public void deberiaTener(int expectedTotal) {
+    public void deberiaTener(int expectedTotal)
+    {
         // TODO
     }
 }
